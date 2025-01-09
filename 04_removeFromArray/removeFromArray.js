73ec14dde1345544ff6ead66e80f6662699afced
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-01-09 15:49:09 +0100

    feat: remove to pass first two tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...whatToRemove) {
+    for (const arg of whatToRemove) {
+        let argIndex = array.lastIndexOf(arg);
+        array.splice(argIndex,1)
+    }
+    return array;
 };
 
 // Do not edit below this line
