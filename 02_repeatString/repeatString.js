65a09953d61ab433bc232c8f28f19d8500c5fc0f
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-01-09 17:26:26 +0100

    feat: do more exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string,repeatFor) {
+    let outputString = ""
+    if (repeatFor < 0) return "ERROR";
+    for(let i = 1; i <= repeatFor; i++) {
+        outputString += string;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(lowerBound, upperBound) {
+    if (upperBound < lowerBound) {
+        newLowerBound = upperBound;
+        newUpperBound = lowerBound;
+    } else {
+        newLowerBound = lowerBound;
+        newUpperBound = upperBound;
+    }
+    if (lowerBound < 0 || upperBound < 0) return "ERROR";
+    if (Math.floor(lowerBound) != lowerBound || 
+       Math.floor(upperBound) != upperBound) 
+       return "ERROR";  
+    if (typeof lowerBound != "number" || typeof upperBound != "number") return "ERROR";
+    let outputSum = 0;
+    for (let i = newLowerBound; i <= newUpperBound; i++) {
+        outputSum += i;
+    }
+    return outputSum;
 };
 
 // Do not edit below this line
