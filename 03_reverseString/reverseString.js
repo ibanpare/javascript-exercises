9f44986e880abd53d2e1aa1135c0b6cf01793660
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-01-09 10:55:50 +0100

    feat: do exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitted = string.split("");
+    let output = [];
+    for(let i = splitted.length - 1; i >= 0 ; i--) {
+        output.push(splitted[i]);
+    }
+    return output.join("");
 };
 
 // Do not edit below this line
