cfc0af91a231d1b88a35768d4aa03c4599271969
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-01-09 17:50:54 +0100

    feat: do exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celsiusTemperature = (temperature - 32) * 5 / 9;
+  return roundedCelsiusTemperature = Math.round(celsiusTemperature * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let fahreneitTemperature = temperature * 9 / 5 + 32;
+  return roundedFahreneitTemperature = Math.round(fahreneitTemperature * 10) / 10;
 };
 
 // Do not edit below this line
