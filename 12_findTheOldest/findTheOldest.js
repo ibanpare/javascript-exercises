6d8f767e8b1e5768de746ba887a3c72c06793d8a
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-02-03 21:13:44 +0100

    feat: solve exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  const currentYear = new Date().getFullYear();
+  const ages = array.map((item) => {
+    if (item.yearOfDeath == undefined) item.yearOfDeath = currentYear;
+    let itemAge = item.yearOfDeath - item.yearOfBirth;
+    return { name: item.name, age: itemAge };
+  });
+  ages.sort((a, b) => b.age - a.age);
+  return ages[0];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+/*
+
+calcola anno attuale che fa sempre comodo
+calcola anno di ognuno con map
+sortali per più grande
+ritorna più grande
+
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+*/
