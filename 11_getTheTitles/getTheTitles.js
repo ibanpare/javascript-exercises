19c5fb4aba5999d534c4263d9e4a3926136ce862
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-02-03 20:51:37 +0100

    feat: solve exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrayOfBooks) {
+    const arrayOfTitles = arrayOfBooks.map(item => item.title);
+    return arrayOfTitles;
 };
 
 // Do not edit below this line
