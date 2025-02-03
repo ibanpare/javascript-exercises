8c1b39af5cd5be88bff2421dd188e17d3c771297
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-02-03 17:05:53 +0100

    feat: solve exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return(a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = array.reduce((sum, item) => sum + item , 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = array.reduce((product, item) => product * item , 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return (a ** b);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+  if(number === 0 || number === 1) return result;
+	while(number > 1) {
+    result *= number;
+    number--;
+  }
+  return result;
 };
 
 // Do not edit below this line
